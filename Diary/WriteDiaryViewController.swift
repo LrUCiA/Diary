106beb2f405183e6dd6f5cf8b74384d7c2f0fc3b
LrUCiA <crosshertz7@gmail.com>
2021-12-24 23:13:38 +0900

    3rd

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by LrUCiA
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+    
+
+}
