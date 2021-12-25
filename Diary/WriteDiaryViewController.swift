489cddfe460ccee1ed9eba56cab4e8cb2c9704f4
LrUCiA <crosshertz7@gmail.com>
2021-12-25 17:05:18 +0900

    4th

--- Diary/WriteDiaryViewController.swift
+++ Diary/WriteDiaryViewController.swift
@@ -8,14 +8,40 @@
 import UIKit
 
 class WriteDiaryViewController: UIViewController {
-
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var contentsTextView: UITextView!
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+   
+    private let datePicker = UIDatePicker()
+    private var diaryDate: Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+        self.configureContentsTextView()
+        
+    }
+    private func configureContentsTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        self.contentsTextView.layer.borderColor = borderColor.cgColor
+        self.contentsTextView.layer.borderWidth = 0.5
+        self.contentsTextView.layer.cornerRadius = 5.0
     }
     
-
-    
-
+    private func configureDatePicker() {
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+    }
+    @IBAction func tapConfirmButton(_ sender: Any) {
+    }
+    @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formatter.string(from: datePicker.date)
+    }
 }
