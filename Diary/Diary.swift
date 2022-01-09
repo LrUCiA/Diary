32e6a54f0cbf494b1797afccc554fa3b831df327
LrUCiA <crosshertz7@gmail.com>
2022-01-09 22:59:33 +0900

    .

--- Diary/Diary.swift
+++ Diary/Diary.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 struct Diary {
-    var title: String
-    var contents: String
-    var date: Date
-    var isStar: Bool
+  var uuidString: String
+  var title: String
+  var contents: String
+  var date: Date
+  var isStar: Bool
 }
+
